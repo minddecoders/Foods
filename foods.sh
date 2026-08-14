3813b8a154d62c122a5b1fb38eac4d104d74cd50
Sidra <sidra.minddecoders@gmail.com>
2026-08-14 18:28:01 +0500

    design done

--- foods.sh
+++ foods.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
-
-echo "🍕 Pizza"  echo "🍔 Burger"
-sleep 2
-
-echo "🍚 Biryani" echo "🍗 Chicken Karahi"
-sleep 2
-
-echo "🍜 Noodles"  echo "🌮 Tacos"
-sleep 2
+WellCome to Food Journey.
 
 
-
-echo "🍟 French Fries" echo "🥪 Sandwich"
-sleep 2
-
-
-echo "🍰 Cake" echo "🍦 Ice Cream"
-sleep 2
-
-
-echo "🍩 Donut" echo "🍎 Apple"
-sleep 2
-
-
-echo "🥭 Mango" echo "🍌 Banana"
-sleep 2
-
-
-echo "🍊 Orange"
