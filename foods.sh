e0ff2122f1baae4124138642e0022dd9973e0626
Sidra <sidra.minddecoders@gmail.com>
2026-08-14 16:01:54 +0500

    version 1.3

--- foods.sh
+++ foods.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
 
-echo "🍕 Pizza"
+echo "🍕 Pizza"  echo "🍔 Burger"
 sleep 2
 
-echo "🍔 Burger"
+echo "🍚 Biryani" echo "🍗 Chicken Karahi"
 sleep 2
 
-echo "🍚 Biryani"
+echo "🍜 Noodles"  echo "🌮 Tacos"
 sleep 2
 
-echo "🍗 Chicken Karahi"
+
+
+echo "🍟 French Fries" echo "🥪 Sandwich"
 sleep 2
 
-echo "🍜 Noodles"
+
+echo "🍰 Cake" echo "🍦 Ice Cream"
 sleep 2
 
-echo "🌮 Tacos"
+
+echo "🍩 Donut" echo "🍎 Apple"
 sleep 2
 
-echo "🍟 French Fries"
+
+echo "🥭 Mango" echo "🍌 Banana"
 sleep 2
 
-echo "🥪 Sandwich"
-sleep 2
-
-echo "🍰 Cake"
-sleep 2
-
-echo "🍦 Ice Cream"
-sleep 2
-
-echo "🍩 Donut"
-sleep 2
-
-echo "🍎 Apple"
-sleep 2
-
-echo "🥭 Mango"
-sleep 2
-
-echo "🍌 Banana"
-sleep 2
 
 echo "🍊 Orange"
